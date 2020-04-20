89c6207bac2479fa3a2ec88396d577fe36908c62
taeeun kim <kimtaeeeny@gmail.com>
2020-04-20 22:45:47 +0900

    style

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -18,7 +18,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(26),
+          maxWidth: rhythm(24),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
